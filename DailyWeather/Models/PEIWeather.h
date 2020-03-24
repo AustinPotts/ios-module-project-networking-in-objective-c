379402139c73230b53a470cfe7f10ee906519517
AustinPotts <pottsevolvements@gmail.com>
2020-03-23 20:49:14 -0400

    Submitting New Project

---
+++ DailyWeather/Models/PEIWeather.h
@@ -0,0 +1,20 @@
+//
+//  PEIWeather.h
+//  DailyWeather
+//
+//  Created by Austin Potts on 3/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PEIWeather : NSObject
+
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DailyWeather/Models/PEIWeather.m
@@ -0,0 +1,13 @@
+//
+//  PEIWeather.m
+//  DailyWeather
+//
+//  Created by Austin Potts on 3/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "PEIWeather.h"
+
+@implementation PEIWeather
+
+@end
